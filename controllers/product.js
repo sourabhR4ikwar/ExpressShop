52579558bb3ac75c418be754d3e75be5e22a3c11
sourabhR4ikwar <sourabh.d.raikwar@gmail.com>
2019-11-19 16:16:52 +0530

    Refactored Code

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require('../models/Product');
 
 exports.viewProduct = (req, res, next) => {
     res.render('add-product', {
---
+++ models/product.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+// const products = [];
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+); s
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title
+    }
+
+    save() {
+        // products.push(this);
+
+        fs.readFile(p, (err, data) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(data);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+
+    }
+
+    static fetchAll(cb) {
+        fs.readFile(p, (err, data) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(data));
+        });
+
+    }
+}
